4cb42b837c2dbb93c116000f27eb72191c03e62a
Sebastian <seb.knutsen@gmail.com>
2018-12-05 18:31:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Infra for monitoring example
+
+## fly command
+my  command
+```
+fly -t pgr301 sp -p anonym-exam -c concourse/pipeline.yml -l credentials.yml
+```
+command template
+```
+fly -t ≤target> sp -p pipeline_name -c concourse/pipeline.yml -l credentials.yml
+```
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+# Modify this file to make it your own :-)
+
+variable "app_prefix" {
+  default = "sebusk"
+}
+
+variable "pipeline_name" {
+  default = "sebusk-pipeline"
+}
+
+variable "statuscake_username" {
+  default = "sebknutsengmailcom"
+}
+
+variable "heroku_email" {
+  default = "seb.knutsen@gmail.com"
+}
